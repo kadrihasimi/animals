dcf66abfcaed92f3c0a97b10c200ae64eec5ed65
kadrihasimi <kadria86@hotmail.com>
2019-03-07 16:11:35 +0100

    App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,38 @@
-import { NgModule } from '@angular/core';
+import {NgModule, ModuleWithProviders} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DragDropModule} from "@angular/cdk/drag-drop";
+import {ScrollingModule} from "@angular/cdk/scrolling";
+import {MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatPaginatorModule, MatProgressSpinnerModule, MatSortModule, MatTableModule} from "@angular/material";
+import {HttpClientModule} from "@angular/common/http";
+
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+      HttpClientModule,
+      DragDropModule,
+      ScrollingModule,
+      MatInputModule,
+      MatPaginatorModule,
+      MatProgressSpinnerModule,
+      MatSortModule,
+      MatTableModule,
+      MatIconModule,
+      MatCardModule,
+      MatFormFieldModule,
+      BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule],
   providers: [
-    StatusBar,
-    SplashScreen,
+      Storage,
+      StatusBar,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Media, MediaObject } from '@ionic-native/media/ngx';
+import { NativeAudio } from '@ionic-native/native-audio/ngx';
+import {Platform} from "@ionic/angular";
 
+
+// http://www.orangefreesounds.com/tag/animal/page/4/
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+    onSuccess : any;
+    onError : any;
+    list:any;
+    constructor(private media: Media,private nativeAudio: NativeAudio,platform: Platform) {
+
+        platform.ready().then(() => {
+        this.list = [
+            {'name':'Maca','id':'1','file':'assets/audio/cat.mp3','img':'assets/animals/cat.jpg'},
+            {'name':'Qeni','id':'2','file':'assets/audio/dog.mp3','img':'assets/animals/dog.jpeg'},
+            {'name':'Lopa','id':'3','file':'assets/audio/cow.wav','img':'assets/animals/cow.jpg'},
+            {'name':'Tigri','id':'4','file':'assets/audio/tiger.wav','img':'assets/animals/tiger.jpg'},
+            {'name':'Majmuni','id':'5','file':'assets/audio/monkey.mp3','img':'assets/animals/monkey.jpg'},
+            {'name':'Luani','id':'6','file':'assets/audio/lion.wav','img':'assets/animals/lion.jpg'},
+            {'name':'Kali','id':'7','file':'assets/audio/horse.wav','img':'assets/animals/horse.jpg'},
+            {'name':'Ujku','id':'8','file':'assets/audio/wolf.mp3','img':'assets/animals/wolf.jpg'},
+            {'name':'Pula','id':'9','file':'assets/audio/chicken.mp3','img':'assets/animals/chicken.jpg'},
+            {'name':'Zogi','id':'10','file':'assets/audio/babychick.mp3','img':'assets/animals/babychick.jpg'},
+            {'name':'Dhija','id':'11','file':'assets/audio/goat.mp3','img':'assets/animals/goat.jpg'},
+            {'name':'Elefanti','id':'12','file':'assets/audio/elephant.mp3','img':'assets/animals/elephant.jpg'},
+        ];
+
+        });
+    }
+
+
+    ngOnInit(){
+        for(let li of this.list) {
+            this.nativeAudio.preloadSimple(li.id, li.file).then(()=>{console.log('Playing')});
+        }
+
+    }
+
+    touchstart(file,id){
+
+        for(let li of this.list){
+
+            if(li.id !==id ){
+                this.nativeAudio.stop(li.id).then(this.onSuccess,this.onError);
+            }
+        }
+        this.nativeAudio.play(id).then(()=>{console.log('Playing')});
+
+    }
+
+
+
+
 
 }
