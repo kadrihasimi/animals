e86b8b1856ea9656237973b94804f8a1ef0baf14
kadrihasimi <kadria86@hotmail.com>
2019-04-08 22:26:03 +0200

    Animals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,10 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {DragDropModule} from "@angular/cdk/drag-drop";
 import {ScrollingModule} from "@angular/cdk/scrolling";
-import {MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatPaginatorModule, MatProgressSpinnerModule, MatSortModule, MatTableModule} from "@angular/material";
 import {HttpClientModule} from "@angular/common/http";
+
+
 
 
 
@@ -19,16 +19,7 @@ import {HttpClientModule} from "@angular/common/http";
   entryComponents: [],
   imports: [
       HttpClientModule,
-      DragDropModule,
       ScrollingModule,
-      MatInputModule,
-      MatPaginatorModule,
-      MatProgressSpinnerModule,
-      MatSortModule,
-      MatTableModule,
-      MatIconModule,
-      MatCardModule,
-      MatFormFieldModule,
       BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule],
   providers: [
       Storage,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,21 +2,21 @@ import {Component, OnInit} from '@angular/core';
 import { Media, MediaObject } from '@ionic-native/media/ngx';
 import { NativeAudio } from '@ionic-native/native-audio/ngx';
 import {Platform} from "@ionic/angular";
-
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
 // http://www.orangefreesounds.com/tag/animal/page/4/
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage implements OnInit{
     onSuccess : any;
     onError : any;
     list:any;
+    loading = false;
     constructor(private media: Media,private nativeAudio: NativeAudio,platform: Platform) {
 
-        platform.ready().then(() => {
         this.list = [
             {'name':'Maca','id':'1','file':'assets/audio/cat.mp3','img':'assets/animals/cat.jpg'},
             {'name':'Qeni','id':'2','file':'assets/audio/dog.mp3','img':'assets/animals/dog.jpeg'},
@@ -30,18 +30,41 @@ export class HomePage implements OnInit{
             {'name':'Zogi','id':'10','file':'assets/audio/babychick.mp3','img':'assets/animals/babychick.jpg'},
             {'name':'Dhija','id':'11','file':'assets/audio/goat.mp3','img':'assets/animals/goat.jpg'},
             {'name':'Elefanti','id':'12','file':'assets/audio/elephant.mp3','img':'assets/animals/elephant.jpg'},
+            {'name':'Arusha','id':'13','file':'assets/audio/bear.mp3','img':'assets/animals/bear.jpg'},
+            {'name':'Delfin','id':'14','file':'assets/audio/dolphin.mp3','img':'assets/animals/dolphin.jpg'},
+            {'name':'Zhaba','id':'15','file':'assets/audio/frog.mp3','img':'assets/animals/frog.jpg'},
+            {'name':'Delja','id':'16','file':'assets/audio/sheep.mp3','img':'assets/animals/sheep.jpg'},
+            {'name':'Gjeli','id':'17','file':'assets/audio/rooster.mp3','img':'assets/animals/rooster.jpg'},
+            {'name':'Pllubi','id':'18','file':'assets/audio/pigeon.mp3','img':'assets/animals/pigeon.jpg'},
+            {'name':'Shqiponja','id':'19','file':'assets/audio/eagle.mp3','img':'assets/animals/eagle.jpg'},
+            {'name':'Gomari','id':'20','file':'assets/audio/donkey.mp3','img':'assets/animals/donkey.jpg'},
+            {'name':'Qurra','id':'21','file':'assets/audio/turkey.mp3','img':'assets/animals/turkey.jpg'},
+            {'name':'Papagalli','id':'22','file':'assets/audio/parrot.mp3','img':'assets/animals/parrot.jpg'},
+            {'name':'Huti','id':'23','file':'assets/audio/owl.mp3','img':'assets/animals/owl.jpg'},
+            {'name':'Kercylli','id':'24','file':'assets/audio/cricket.mp3','img':'assets/animals/cricket.jpg'},
+            {'name':'Dreri','id':'25','file':'assets/audio/deer.mp3','img':'assets/animals/deer.jpg'},
+            {'name':'Pata','id':'26','file':'assets/audio/duck.mp3','img':'assets/animals/duck.jpg'},
+            {'name':'Sorra','id':'27','file':'assets/audio/crow.mp3','img':'assets/animals/crow.jpg'},
+            {'name':'Seagulls','id':'28','file':'assets/audio/seagulls.mp3','img':'assets/animals/seagulls.jpg'},
+            {'name':'Sparrow','id':'29','file':'assets/audio/sparrow.mp3','img':'assets/animals/sparrow.jpg'},
+            {'name':'Peacock','id':'30','file':'assets/audio/peacock.mp3','img':'assets/animals/peacock.gif'},
+
         ];
 
-        });
+
     }
 
 
     ngOnInit(){
+
         for(let li of this.list) {
             this.nativeAudio.preloadSimple(li.id, li.file).then(()=>{console.log('Playing')});
         }
-
+        setTimeout(() => {
+            this.loading = true;
+        }, 5000);
     }
+
 
     touchstart(file,id){
 
